8f130c7e46f15344ddc228b63e3aa603f4b3a8cf
NIKITA AD <nikita8ad@gmail.com>
2018-07-23 12:35:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ function deleteFromObjectByKey(object, key){
   return newObj;
 }
   
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
+}  
   
-  
-  
-  
-  
-  
-  
-  
-  
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
+}  
